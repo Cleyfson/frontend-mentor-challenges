5e2f45eb162ff7fc9bcdfcb22c6b178c7230a2cd
Cleifson Araujo <cl.raujo1@gmail.com>
2022-10-27 11:20:53 -0300

    create booklist

--- react-bookstore/src/components/BookList/BookList.jsx
+++ react-bookstore/src/components/BookList/BookList.jsx
@@ -1,33 +1,46 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.scss';
 
 const BookList = () => {
-  const getBooks = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=rRpiMuxqPVRLxWEP5rfyx6Wan2oQAopx`
-      );
-      const data = response.data;
-      const results = data.results;
+	const [books, setBooks] = useState([]);
 
-      console.log(results.lists[0].books);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const getBooks = async () => {
+		try {
+			const response = await axios.get(
+				`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=rRpiMuxqPVRLxWEP5rfyx6Wan2oQAopx`
+			);
+			const data = await response.data;
+			const results = await data.results;
 
-  useEffect(() => {
-    getBooks();
-    console.log('hello');
-  }, []);
+			console.log(results.lists[0].books);
+			setBooks(results.lists[0].books);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className='booklist'>
-      <h1>BookList</h1>
-    </div>
-  );
+	useEffect(() => {
+		getBooks();
+	}, []);
+
+	return (
+		<div className='booklist'>
+			{books.map((book) => {
+				return (
+					<div key={book.primary_isbn10} className='book'>
+						<div>
+							<img className='book__image' src={book.book_image} alt='/' />
+							<h4 className='book__title'>{book.title}</h4>
+							<h5 className='book__author'>{book.author}</h5>
+						</div>
+						<button>Add to cart</button>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default BookList;
