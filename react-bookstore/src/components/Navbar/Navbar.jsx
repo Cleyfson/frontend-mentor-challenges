5086bd39075fea3cad5e8ac6aaf8b40911076cf2
Cleifson Araujo <cl.raujo1@gmail.com>
2022-11-05 19:01:51 -0300

    navbar change

--- react-bookstore/src/components/Navbar/Navbar.jsx
+++ react-bookstore/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 const Navbar = () => {
 	return (
 		<div className='navbar' datatype='nav-bar'>
-			<h2 className='navbar__logo'>BookStore</h2>
+			<Link className='navbar__logo' to={'/'}>
+				<h2>BookStore</h2>
+			</Link>
 			<AiOutlineShoppingCart className='navbar__cart' />
 		</div>
 	);
